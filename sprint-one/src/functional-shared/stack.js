ad6bd5dcb9b221a6fb352ad77ae0e1dc3adc1c4d
James Reynolds <jameshreynolds@gmail.com>
2018-01-17 14:55:46 -0800

    Complete stack function share pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+  	count: 0,
+	  storage: {}
+  };
+  _.extend(someInstance, stackMethods)
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    var popped = this.storage[this.count -1];
+    if (this.count !== 0) {
+    	delete this.storage[this.count];
+      this.count--;
+    }
+    return popped;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
