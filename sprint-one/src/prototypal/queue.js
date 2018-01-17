9a91070112b837837d6d93832699c3e4e498ea71
Chazie9 <charlesharring3@gmail.com>
2018-01-17 15:45:44 -0800

    got the first test working for Queue Protoypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.index1 = 0;
+  someInstance.index2 = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+enqueue: function(data) {
+
+},
+dequeue: function(data) {
+
+},
+size: function(data) {
+  return this.index1 - this.index2;
+}
+
+
+
+
+};
 
 
