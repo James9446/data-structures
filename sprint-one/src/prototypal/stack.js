c88d496e4db989c2413de874e98ae82e2710985b
Chazie9 <charlesharring3@gmail.com>
2018-01-17 15:34:46 -0800

    got the first test working for the prototypal pattern (stack)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
-var Stack = function() {
+var Stack = function(data) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage =  {}; 
+  someInstance.count =  0; 
+  
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(data) {
+
+  },
+  pop: function(data) {
+
+  },
+  size: function(data) {
+    return this.count;
+  }
+};
+
+//var testStack = Stack("a");
 
