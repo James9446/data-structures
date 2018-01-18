5e02b361f3b0a7f8101653265e8da509988d8745
James9446 <jameshreynolds@gmail.com>
2018-01-18 11:41:36 -0800

    finished first test for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,8 +2,31 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.count = 0;
+  //storage = {};
+  // var storage = {};
 
   list.addToTail = function(value) {
+    debugger;
+    if(Object.keys(list).length === 6) {
+    // console.log('is empty');
+    // console.log('list:', list);
+      var theNode = Node(value);
+      this[this.count] = theNode;
+      
+      list.head = this[this.count];
+      // console.log('list.head:', list.head);
+      // console.log('list[this[this.count]]:', this[this[this.count]]);
+      list.tail = this[this.count];
+    } else {
+      // debugger;
+      console.log('not empty');
+      var lastNode = Node(value);
+      this[this.count] = lastNode;
+      list.tail.next = this[this.count];
+      list.tail = this[this.count]; 
+    }
+    this.count++;
   };
 
   list.removeHead = function() {
