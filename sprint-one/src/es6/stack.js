cc36ca37b96b7488259b22b804c30a69b5ff6bbc
Chazie9 <charlesharring3@gmail.com>
2018-01-17 16:26:17 -0800

    got es6 stack class set up

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, count) {
+    this.storage = {},
+    this.count = 0
+  }
+
+  push(data) {
+    this.storage[this.count] = data;
+    this.count++;
+  }
+  pop() {
+    if(this.count > 0) {
+      this.count--;
+      var stringToReturn = this.storage[this.count];
+      delete this.storage[this.count];
+      return stringToReturn;
+    }
+  }
+  size() {
+    return this.count;
   }
 
 }
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {},
+  this.index1 = 0,
+  this.index2 = 0;
 };
+
+Queue.prototype.enqueue = function(data) {
+  this.index1++;
+  this.storage[this.index1] = data;
+};
+
+Queue.prototype.dequeue = function() {
+  if(this.index1 !== this.index2) {
+    this.index2++;
+    var stringToReturn = this.storage[this.index2];
+    delete this.storage[this.index2];
+    return stringToReturn;
+    
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.index1 - this.index2;
+}
+
+
 
 
