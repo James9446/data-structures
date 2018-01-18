3e54c9537ce8ff769a370dd9906cfebf57a7c56a
James Reynolds <jameshreynolds@gmail.com>
2018-01-17 16:19:16 -0800

    Complete protoypal pattern Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.count = 0;
+  newInstance.storage = {};
+  return newInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.storage[this.count] = value;
+	this.count++;
+}
+queueMethods.dequeue = function() {
+  var newOrder = 0;
+  var dequeued = this.storage[0];
+  delete this.storage[0];
+  this.count--;
+  for (var key in this.storage) {
+    this.storage[newOrder] = this.storage[key];
+    delete this.storage[key];
+    newOrder++
+  }
+  return dequeued;
+}
 
+queueMethods.size = function() {
+  var size = Object.keys(this.storage);
+  return size.length;
+}
