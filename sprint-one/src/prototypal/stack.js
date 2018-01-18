8937568ff9606008db8bbee1280ff32e00594d11
James Reynolds <jameshreynolds@gmail.com>
2018-01-17 16:13:36 -0800

    Complete pototypal pattern stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(stackMethods);
+  newInstance.count = 0;
+  newInstance.storage = {};
+  return newInstance;
 };
-
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function() {
+ var popped = this.storage[this.count -1];
+ if (this.count !== 0) {
+ 	delete this.storage[this.count];
+   this.count--;
+ }
+ return popped;
+};
+
+stackMethods.size = function() {
+	return this.count;
+}
 
