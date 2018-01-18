dd5a96392cfbd450c60f8a648561a022c7b51553
Chazie9 <charlesharring3@gmail.com>
2018-01-17 16:10:01 -0800

    finsihed stack pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -8,12 +8,18 @@ var Stack = function() {
 };
 
 
-Stack.prototype.push = function() {
-
+Stack.prototype.push = function(data) {
+  this.storage[this.count] = data;
+  this.count++;
 };
 
 Stack.prototype.pop = function() {
-
+  if(this.count > 0) {
+    this.count--;
+    var stringToReturn = this.storage[this.count];
+    delete this.storage[this.count];
+    return stringToReturn;
+  }
 };
 
 Stack.prototype.size = function() {
