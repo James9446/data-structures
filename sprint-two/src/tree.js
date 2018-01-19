29734851c7b471d211863a4c9d5aecd82b865560
James9446 <jameshreynolds@gmail.com>
2018-01-18 23:23:19 -0800

    got all tests green for tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,12 @@
 var Tree = function(value) {
+  
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +14,39 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  // console.log('contains this', this);
+  var doesHave = false;
+  var doesContain = function (node, target) {
+    // console.log('doesContains this', this);
+    // isTrue = true;
+    //debugger;
+    console.log('node.value:', node.value);
+    console.log('target:', target);
+    if (node.value === target) {
+      console.log('condition passed');
+      return doesHave = true; 
+    }
+    if (node.children.length > 0) {
+      node.children.forEach(function(child) {
+        doesContain(child, target);
+      });
+      // for (var i = 0; i < node.children.length; i++) {
+      //   doesContain(node.children[i], target);
+      // }
+    }
+    
+  };
+  // debugger;
+  doesContain(this, target);
+  // if (doesHave) {
+  //   return true;
+  // }
+  return doesHave; 
 };
 
 
